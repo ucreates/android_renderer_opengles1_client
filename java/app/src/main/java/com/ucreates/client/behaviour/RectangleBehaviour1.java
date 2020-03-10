8e86bdd67cfec633a846b7c215f95b5bd29f1fbd
ucreates <ucreates.inc@gmail.com>
2020-03-11 00:16:37 +0900

    add rectangle1 demo

---
+++ java/app/src/main/java/com/ucreates/client/behaviour/RectangleBehaviour1.java
@@ -0,0 +1,36 @@
+// ======================================================================
+// Project Name    : android_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.client.behaviour;
+import android.content.Context;
+import com.frontend.behaviour.BaseBehaviour;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.polygon.RectangleAsset1;
+import com.ucreates.renderer.entity.GLESColor;
+public class RectangleBehaviour1 extends BaseBehaviour {
+    public BaseAsset asset;
+    public RectangleBehaviour1(Context context) {
+        super(context);
+        this.asset = new RectangleAsset1(1, 1, GLESColor.white);
+        this.asset.create();
+        return;
+    }
+    @Override
+    public void onCreate() {
+        return;
+    }
+    @Override
+    public void onUpdate(double delta) {
+        this.asset.transform.setPosition(0.0f, 0.0f, 0.0f);
+        this.asset.transform.setScale(1.0f, 1.0f, 1.0f);
+        this.asset.transform.setRotation(0.0f, 0.0f, 0.0f);
+        this.asset.vertex.setRandomColor();
+        return;
+    }
+}
