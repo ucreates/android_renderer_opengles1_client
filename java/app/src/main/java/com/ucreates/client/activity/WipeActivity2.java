b36ae68106099b147eb039b1f778c3d72833bbdc
ucreates <ucreates.inc@gmail.com>
2020-04-25 00:22:03 +0900

    add wipe2D/3D demo

---
+++ java/app/src/main/java/com/ucreates/client/activity/WipeActivity2.java
@@ -0,0 +1,78 @@
+// ======================================================================
+// Project Name    : android_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.client.activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.renderscript.Float3;
+import android.support.v7.app.AppCompatActivity;
+import com.ucreates.client.R;
+import com.ucreates.client.behaviour.CubeBehaviour1;
+import com.ucreates.renderer.asset.wipe.WipeAsset1;
+import com.ucreates.renderer.entity.GLESColor;
+import com.ucreates.renderer.renderer.GLES1Renderer;
+import com.ucreates.renderer.timer.TimeInterval;
+import java.util.ArrayList;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+public class WipeActivity2 extends AppCompatActivity implements GLSurfaceView.Renderer {
+    private GLES1Renderer renderer;
+    private WipeAsset1 wipe;
+    private long previewTime;
+    private ArrayList<CubeBehaviour1> behaviours;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        GLSurfaceView view = (GLSurfaceView) this.findViewById(R.id.view);
+        view.setEGLContextClientVersion(1);
+        view.setEGLConfigChooser(8, 8, 8, 8, 16, 1);
+        view.setPreserveEGLContextOnPause(true);
+        view.setRenderer(this);
+        return;
+    }
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Context context = this.getApplicationContext();
+        this.behaviours = new ArrayList<CubeBehaviour1>();
+        for (int i = 0; i < 1; i++) {
+            this.behaviours.add(new CubeBehaviour1(context));
+        }
+        this.renderer = new GLES1Renderer();
+        this.renderer.create();
+        this.renderer.camera.setClear(GLESColor.black);
+        this.renderer.camera.setClippingPlane(0.1f, 100.0f);
+        this.renderer.camera.setFOV(60.0f);
+        this.renderer.camera.setLookAt(new Float3(0.0f, 0.0f, -5.0f), new Float3(0.0f, 0.0f, 0.0f), new Float3(0.0f, 1.0f, 0.0f));
+        this.wipe = new WipeAsset1(0.5f, 100, 5.0f);
+        this.wipe.create(GLES1Renderer.DIMENSION3D);
+        this.previewTime = System.currentTimeMillis();
+        return;
+    }
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        this.renderer.rebind(this.getApplicationContext(), width, height);
+        return;
+    }
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        long currentTime = System.currentTimeMillis();
+        long delta = currentTime - this.previewTime;
+        this.previewTime = currentTime;
+        this.renderer.transform(gl, GLES1Renderer.DIMENSION3D);
+        this.renderer.render(this.wipe, GLES1Renderer.WIPEIN, delta, 2000);
+        for (int i = 0; i < this.behaviours.size(); i++) {
+            CubeBehaviour1 behaviour = this.behaviours.get(i);
+            behaviour.onUpdate(delta);
+            this.renderer.render(behaviour.asset);
+        }
+        return;
+    }
+}
