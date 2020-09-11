e9bd94d02ca2e494232d754fafe3d8ca0c1af2bd
ucreates <ucreates.inc@gmail.com>
2020-09-12 00:10:02 +0900

    add circle3 demo

---
+++ java/app/src/main/java/com/ucreates/client/behaviour/CircleBehaviour3.java
@@ -0,0 +1,36 @@
+// ======================================================================
+// Project Name    : android_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.client.behaviour;
+import android.content.Context;
+import com.frontend.behaviour.BaseBehaviour;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.mesh.CircleAsset3;
+import com.ucreates.renderer.entity.GLESColor;
+public class CircleBehaviour3 extends BaseBehaviour {
+    public BaseAsset asset;
+    public CircleBehaviour3(Context context) {
+        super(context);
+        this.asset = new CircleAsset3(0.5f, 100, GLESColor.white);
+        this.asset.create();
+        return;
+    }
+    @Override
+    public void onCreate() {
+        return;
+    }
+    @Override
+    public void onUpdate(double delta) {
+        this.asset.transform.setPosition(0.0f, 0.0f, 0.0f);
+        this.asset.transform.setScale(1.0f, 1.0f, 1.0f);
+        this.asset.transform.setRotation(0.0f, 0.0f, 0.0f);
+        this.asset.vertex.setRandomColor();
+        return;
+    }
+}
