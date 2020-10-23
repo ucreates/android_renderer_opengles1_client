5a82bb397cdf123f2147ee45ce5ffa34b98ab9d2
ucreates <ucreates.inc@gmail.com>
2020-10-24 00:09:38 +0900

    add cube3 texture demo

---
+++ java/app/src/main/java/com/ucreates/client/behaviour/TextureBehaviour21.java
@@ -0,0 +1,37 @@
+// ======================================================================
+// Project Name    : android_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.client.behaviour;
+import android.content.Context;
+import com.frontend.behaviour.BaseBehaviour;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.mesh.CubeAsset3;
+import com.ucreates.renderer.entity.GLESColor;
+public class TextureBehaviour21 extends BaseBehaviour {
+    public BaseAsset asset;
+    private float rotate;
+    public TextureBehaviour21(Context context) {
+        super(context);
+        this.asset = new CubeAsset3(1, 1, 1, GLESColor.white);
+        this.asset.create("texture01.jpg", context);
+        return;
+    }
+    @Override
+    public void onCreate() {
+        return;
+    }
+    @Override
+    public void onUpdate(double delta) {
+        this.asset.transform.setPosition(0.0f, 0.0f, 0.0f);
+        this.asset.transform.setScale(1.0f, 1.0f, 1.0f);
+        this.asset.transform.setRotation(this.rotate, this.rotate, this.rotate);
+        this.rotate += 1.0f;
+        return;
+    }
+}
