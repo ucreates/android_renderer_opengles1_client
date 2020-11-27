59d7ca91ea2d029f5a0d9a27b13a46a28038854f
ucreates <ucreates.inc@gmail.com>
2020-11-28 00:13:09 +0900

    add cube4 demo

---
+++ java/app/src/main/java/com/ucreates/client/behaviour/CubeBehaviour4.java
@@ -0,0 +1,38 @@
+// ======================================================================
+// Project Name    : android_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+package com.ucreates.client.behaviour;
+import android.content.Context;
+import com.frontend.behaviour.BaseBehaviour;
+import com.ucreates.renderer.asset.BaseAsset;
+import com.ucreates.renderer.asset.mesh.CubeAsset4;
+import com.ucreates.renderer.entity.GLESColor;
+public class CubeBehaviour4 extends BaseBehaviour {
+    public BaseAsset asset;
+    private float rotate;
+    public CubeBehaviour4(Context context) {
+        super(context);
+        this.asset = new CubeAsset4(1, 1, 1, GLESColor.white);
+        this.asset.create();
+        return;
+    }
+    @Override
+    public void onCreate() {
+        return;
+    }
+    @Override
+    public void onUpdate(double delta) {
+        this.asset.transform.setPosition(0.0f, 0.0f, 0.0f);
+        this.asset.transform.setScale(1.0f, 1.0f, 1.0f);
+        this.asset.transform.setRotation(this.rotate, this.rotate, this.rotate);
+        this.asset.vertex.setRandomColor();
+        this.rotate += 1.0f;
+        return;
+    }
+}
